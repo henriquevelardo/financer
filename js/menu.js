db217b4463fcfd67c6fd2e9b9702183291682f18
Henrique Velardo <Henrique>
2023-02-24 14:31:47 -0300

    criando funcionalidade do menu hamburguer

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const imagemMenu = document.querySelector("[data-imagemMenu]")
+const menu = document.querySelector("[data-menu]")
+
+imagemMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu--ativo')
+})
